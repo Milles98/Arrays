9770d7a8df17f388e782e8a5b609c4123d6adf42
Mille Elfver <mille-elfver98@hotmailc.om>
2023-09-14 10:36:27 +0200

    01 new int array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //// 1. Deklarera
+            //int[] scores;
+
+            //// 2. Skapa
+            //scores = new int[10];
+
+            int[] numbers = new int[4];
+            numbers[0] = 1;
+            numbers[1] = 3;
+            numbers[2] = 45;
+            numbers[3] = 5;
+
+            Console.WriteLine(numbers[2]);
+            Console.ReadLine();
         }
     }
 }
